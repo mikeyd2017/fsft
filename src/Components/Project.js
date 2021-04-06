10b95c5a39568458a68e641a5e3f89816b17c6aa
mikeyd2017 <dambrosiomichael@rocketmail.com>
2021-04-06 16:14:39 -0500

    image placeholder for projects

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -2,6 +2,11 @@ export default function Project(props) {
     return (
         <div className="project-content project-fade-in" style={{ height: props.height, width: props.width}}>
             <h2 className="project-content-name">{props.name}</h2>
+            <div className="project-content__imagecontainer">
+                <div className="project-content__imagecontainer-image">
+                    
+                </div>
+            </div>
             <p className="project-content-description">{props.description}</p>
         </div>
     );
